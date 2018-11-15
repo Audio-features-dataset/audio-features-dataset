fe8c8032d6a90d6f933f72f5eb1dabed9e5cf637
vbommena <vishaal.bommena@gmail.com>
2018-11-14 22:31:16 -0500

    changes made

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import csv
 import pandas as pd
 import numpy as np
-
 def main():
     data = pd.read_csv("/Users/vishaalbommena/Desktop/year_prediction.csv", sep=',', quotechar='"', header=0)
     data = data[['label', 'TimbreAvg1', 'TimbreAvg2', 'TimbreAvg3', 'TimbreAvg4', 'TimbreAvg5', 'TimbreCovariance1', 'TimbreCovariance2', 'TimbreCovariance3', 'TimbreCovariance4', 'TimbreCovariance5']]
@@ -10,7 +9,6 @@ def main():
     train = X[:int(0.6*len(X))]
     validation = X[int(0.6*len(X)) + 1: int(0.6*len(X)) + int(0.2*len(X))]
     test = X[int(0.6*len(X)) + int(0.2*len(X)) + 1: int(0.6*len(X)) + int(0.2*len(X)) + int(0.2*len(X))]
-    print "train: ", len(train)
-    print "validation: ", len(validation)
-    print "test: ", len(test)
+    return train, validation, test
+
 main()
