1403c9aeb7cf97b0da4aad079a413cf9fd60ede4
vbommena <vishaal.bommena@gmail.com>
2018-11-14 22:18:16 -0500

    main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import csv
+import pandas as pd
+import numpy as np
+
+def main():
+    data = pd.read_csv("/Users/vishaalbommena/Desktop/year_prediction.csv", sep=',', quotechar='"', header=0)
+    data = data[['label', 'TimbreAvg1', 'TimbreAvg2', 'TimbreAvg3', 'TimbreAvg4', 'TimbreAvg5', 'TimbreCovariance1', 'TimbreCovariance2', 'TimbreCovariance3', 'TimbreCovariance4', 'TimbreCovariance5']]
+    X = data.as_matrix()
+    X = X[:int(0.5*len(X))]
+    print X
+    print "len(X): ", len(X)
+main()
