960d20337e261285b13327a86474a64cc210984a
vbommena <vishaal.bommena@gmail.com>
2018-11-14 22:21:33 -0500

    reduced total dataset

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ def main():
     data = pd.read_csv("/Users/vishaalbommena/Desktop/year_prediction.csv", sep=',', quotechar='"', header=0)
     data = data[['label', 'TimbreAvg1', 'TimbreAvg2', 'TimbreAvg3', 'TimbreAvg4', 'TimbreAvg5', 'TimbreCovariance1', 'TimbreCovariance2', 'TimbreCovariance3', 'TimbreCovariance4', 'TimbreCovariance5']]
     X = data.as_matrix()
-    X = X[:int(0.5*len(X))]
+    X = X[:int(0.01*len(X))]
     print X
     print "len(X): ", len(X)
+
+    
 main()
