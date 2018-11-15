e132fc8c2b5e805e268a55e885a3f256920e5667
vbommena <vishaal.bommena@gmail.com>
2018-11-15 18:42:11 -0500

    Changes made to svm model

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     data = pd.read_csv("/Users/vishaalbommena/Desktop/year_prediction.csv", sep=',', quotechar='"', header=0)
     data = data[['label', 'TimbreAvg1', 'TimbreAvg2', 'TimbreAvg3', 'TimbreAvg4', 'TimbreAvg5', 'TimbreCovariance1', 'TimbreCovariance2', 'TimbreCovariance3', 'TimbreCovariance4', 'TimbreCovariance5']]
     X = data.as_matrix()
-    X = X[:1000]
+    X = X[:int(0.01*len(X))]
     '''
     train = X[:int(0.6*len(X))]
     validation = X[int(0.6*len(X)) + 1: int(0.6*len(X)) + int(0.2*len(X))]
