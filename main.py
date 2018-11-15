547cba867ce369056511d8aea261a76969061916
vbommena <vishaal.bommena@gmail.com>
2018-11-14 22:39:00 -0500

    removed changes

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ def main():
     data = data[['label', 'TimbreAvg1', 'TimbreAvg2', 'TimbreAvg3', 'TimbreAvg4', 'TimbreAvg5', 'TimbreCovariance1', 'TimbreCovariance2', 'TimbreCovariance3', 'TimbreCovariance4', 'TimbreCovariance5']]
     X = data.as_matrix()
     X = X[:int(0.01*len(X))]
+    '''
     train = X[:int(0.6*len(X))]
     validation = X[int(0.6*len(X)) + 1: int(0.6*len(X)) + int(0.2*len(X))]
     test = X[int(0.6*len(X)) + int(0.2*len(X)) + 1: int(0.6*len(X)) + int(0.2*len(X)) + int(0.2*len(X))]
+    '''
     return train, validation, test
 
 main()
