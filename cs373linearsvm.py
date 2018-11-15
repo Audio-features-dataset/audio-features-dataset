c844a5b60f81b554ea1447525890794d357f2525
vbommena <vishaal.bommena@gmail.com>
2018-11-15 18:41:51 -0500

    Changes made to svm model

--- cs373linearsvm.py
+++ cs373linearsvm.py
@@ -3,36 +3,42 @@ import main as data
 from random import randint
 from sklearn import svm
 from operator import itemgetter
-SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
-    decision_function_shape='ovr', degree=3, gamma='scale', kernel='linear',
-    max_iter=-1, probability=False, random_state=None, shrinking=True,
-    tol=0.001, verbose=False)
 
-clf = svm.SVC()
+clf = svm.LinearSVC()
+clf1 = svm.SVC(kernel='rbf')
 
-def main():
+def main(opt):
     t_d = data.main()
     X = t_d[:, 1:len(t_d[0])]
     y = t_d[:, 0]
-
-    X_train = X[:0.6*len(X)]
-    y_train = X[:0.6*len(y)]
-
-    ml_train(np.array(X_train), np.array(y_train), clf)
-
-    X_pred = X[0.6*len(X) + 1:]
-    y_pred = y[0.6*len(X) + 1:]
-
-    predictions = ml_predict(np.array(X_pred), np.array(y_pred), clf)
+    X_train = X[:int(0.6*len(X))]
+    y_train = y[:int(0.6*len(y))]
+    if opt == 1:
+        ml_train(np.array(X_train), np.array(y_train), clf)
+    else:
+        ml_train(np.array(X_train), np.array(y_train), clf1)
+    X_pred = X[int(0.6*len(X)) + 1:]
+    y_pred = y[int(0.6*len(X)) + 1:]
+    predictions = []
+    if opt == 1:
+        predictions = ml_predict(np.array(X_pred), np.array(y_pred), clf)
+    else:
+        predictions = ml_predict(np.array(X_pred), np.array(y_pred), clf1)
     print predictions
-    #accuracy = (X_pred - X_train)/len(y)
+    accuracy = 0
+    for i in range(0, len(predictions)):
+        if predictions[i] == y_pred[i]:
+            accuracy = accuracy + 1
+    print accuracy
 
 def ml_train(X, y, clf):
-    clf.fit(np.array(X).reshape(-1, 1), np.array(y))
-    
+    print "Training"
+    clf.fit(X, y)
+
 def ml_predict(X_pred, y_test, clf):
+    print "Prediction"
     expected = y_test
     predicted = clf.predict(X_pred)
     return predicted
 
-main()
+main(1)
